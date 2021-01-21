ea00a7a2776e965e331baada0a042c7c8b00119c
zhixiang.xiao <1419958382@qq.com>
2021-01-21 20:28:11 +0800

    LeetCode:面试题 17.16. 按摩师

--- src/com/leetcode/xzx/dynamic/Message.java
+++ src/com/leetcode/xzx/dynamic/Message.java
@@ -17,11 +17,11 @@ public class Message {
 		int[][] dp = new int[nums.length][2];
 		dp[0][0] = 0; // 第一个没接
 		dp[0][1] = nums[0]; // 第一个接了
-		for(int i = 1; i<nums.length; i++) {
-			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
-			dp[i][1]=dp[i-1][0]+nums[i];
+		for (int i = 1; i < nums.length; i++) {
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+			dp[i][1] = dp[i - 1][0] + nums[i];
 		}
-		return 0;
+		return Math.max(dp[nums.length-1][0], dp[nums.length-1][1]);
 	}
 
 }
