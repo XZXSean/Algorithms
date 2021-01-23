466265069c524b3ed9b0ea3ec4113714ec090181
zhixiang.xiao <1419958382@qq.com>
2021-01-23 18:48:18 +0800

    746. 使用最小花费爬楼梯

--- src/com/leetcode/xzx/dynamic/ClimbStairs.java
+++ src/com/leetcode/xzx/dynamic/ClimbStairs.java
@@ -12,26 +12,55 @@ import java.util.Scanner;
 
 public class ClimbStairs {
 
-    public static void main(String[] args) {
-        Scanner inScanner = new Scanner(System.in);
-        while (inScanner.hasNext()) {
-            int num = inScanner.nextInt();
-            System.out.println(climbStairs(num));
+	public static void main(String[] args) {
+		int[] cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+		System.out.println(minCostClimbingStairs(cost));
+//		Scanner inScanner = new Scanner(System.in);
+//		while (inScanner.hasNext()) {
+//			int num = inScanner.nextInt();
+//			System.out.println(climbStairs(num));
+//		}
+	}
 
-        }
-    }
+	/**
+	 * 746. 使用最小花费爬楼梯: https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+	 * 
+	 * 长度为 n+1 的数组 dp用来保存每个位置的最小花费
+	 * 
+	 * @param cost
+	 * @return
+	 */
+	public static int minCostClimbingStairs(int[] cost) {
+		int n = cost.length;
+		int[] dp = new int[n + 1];
+		dp[0] = dp[1] = 0;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+		}
+		return dp[n];
+	}
 
-    public static int climbStairs(int n) {
-        int[] nums = new int[n + 1];
-        return climbStairsMemo(n, nums);
-    }
+	/**
+	 * 70. 爬楼梯: https://leetcode-cn.com/problems/climbing-stairs/
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static int climbStairs(int n) {
+		int[] nums = new int[n + 1];
+		return climbStairsMemo(n, nums);
+	}
 
-    public static int climbStairsMemo(int n, int[] memo) {
-        if (memo[n] > 0) return memo[n];
-        if (n == 1) memo[1] = 1;
-        else if (n == 2) memo[2] = 2;
-        else memo[n] = climbStairsMemo(n - 1, memo) + climbStairsMemo(n - 2, memo);
-        return memo[n];
-    }
+	public static int climbStairsMemo(int n, int[] memo) {
+		if (memo[n] > 0)
+			return memo[n];
+		if (n == 1)
+			memo[1] = 1;
+		else if (n == 2)
+			memo[2] = 2;
+		else
+			memo[n] = climbStairsMemo(n - 1, memo) + climbStairsMemo(n - 2, memo);
+		return memo[n];
+	}
 
 }
